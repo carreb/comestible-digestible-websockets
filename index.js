1905aa76258e695393573f3cdaa5cddf7cef46fd
carreb <caleb@domyjobfor.me>
2024-03-22 12:40:57 -0400

    add flickering

--- index.js
+++ index.js
@@ -14,7 +14,31 @@ function decreaseTimer() {
     if (timer > 0) {
         timer--;
         io.emit("timer", timer);
+    } else if (timer == 0 || timer == -1) {
+        flickerTimer()
+        timer = -2
     }
+}
+
+function flickerTimer() {
+    // send alternating timer values of -1 and 0 to flicker the timer on the client
+    // the client hides the timer on value -1, so it should start visible, flicker on and off three times, and end invisible.
+    io.emit("timer", -1);
+    setTimeout(() => {
+        io.emit("timer", 0);
+    }, 500);
+    setTimeout(() => {
+        io.emit("timer", -1);
+    }, 1000);
+    setTimeout(() => {
+        io.emit("timer", 0);
+    }, 1500);
+    setTimeout(() => {
+        io.emit("timer", -1);
+    }, 2000);
+    setTimeout(() => {
+        io.emit("timer", -2);
+    }, 2500);
 }
 
 setInterval(decreaseTimer, 1000);
